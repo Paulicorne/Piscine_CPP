1dda19cacdcab8bc0d1324cba08f3bc5c78ba16e
Pauline Millet <milletp.pro@gmail.com>
2022-04-26 11:40:56 +0200

    finished cpp08

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -3,22 +3,38 @@
 
 # include <iostream>
 # include <string>
+# include <stack> // already includes std::deque<T> by default
+# include <list>
 
-class MutantStack
+// template <typename T, class Container = std::deque<T> > // I can specify what underlying container I use, here a "deque"
+// template <typename T, class Container = std::list<T> > // here my MutantStack will use a "list"
+template <typename T> // here, my MutantStack will use a "deque" by default
+class MutantStack : public std::stack<T> // I can have any type stored inside my MutantStack
 {
 
 	public:
 
-		MutantStack();
-		MutantStack( MutantStack const & src );
-		~MutantStack();
+		MutantStack(void){};
+		MutantStack( MutantStack const & src ){
+			*this = src;
+		};
+		~MutantStack(){};
 
-		MutantStack &		operator=( MutantStack const & rhs );
+		MutantStack &		operator=( MutantStack const & rhs ){
+			if ( *this != &rhs )
+			{
+				*this = new std::stack<T>(rhs);
+			}
+			return *this;
+		};
+
+		typedef typename std::stack<T>::container_type::iterator iterator; // increasing readability of code
+
+		iterator begin(){return std::stack<T>::c.begin();}; // "c" is the underlying container, protected in original class, but useable in inherited classes
+		iterator end(){return std::stack<T>::c.end();};
 
 	private:
 
 };
-
-std::ostream &			operator<<( std::ostream & o, MutantStack const & i );
 
 #endif /* ***************************************************** MUTANTSTACK_H */
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,13 +1,27 @@
 #include "MutantStack.hpp"
+#include <list>
+
+#define END "\033[0m"
+#define GREEN "\033[32m"
 
 int main()
 {
+	std::cout << GREEN << "Subject's main :" << END << std::endl;
+	/* SUBJECT'S MAIN */
+
 	MutantStack<int> mstack;
+
 	mstack.push(5);
 	mstack.push(17);
+
+	//std::cout << "Last value added is : "; // uncomment for more readability
 	std::cout << mstack.top() << std::endl;
+	//std::cout << "Removing \"17\" (last added) from our MutantStack" << std::endl; // uncomment for more readability
 	mstack.pop();
+	//std::cout << "MutantStack has currently size : "; // uncomment for more readability
 	std::cout << mstack.size() << std::endl;
+	std::cout << std::endl;
+
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
@@ -17,11 +31,52 @@ int main()
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
+	//std::cout << "MutantStack now contains : " << std::endl; // uncomment for more readability
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
-	std::stack<int> s(mstack);
+	std::cout << std::endl;
+
+
+
+	/* COMPARING */
+	std::cout << GREEN << "Creating a simple MutantStack :" << END << std::endl;
+	MutantStack<int> mstack2;
+
+	mstack2.push(1);
+	mstack2.push(2);
+	mstack2.push(3);
+	mstack2.push(4);
+
+	MutantStack<int>::iterator it2;
+	MutantStack<int>::iterator ite2 = mstack2.end();
+	for (it2 = mstack2.begin(); it2 != ite2; it2++)
+	{
+		std::cout << *it2 << std::endl;
+	}
+	std::stack<int> s2(mstack2);
+	std::cout << std::endl;
+
+
+
+	/* SAME WITH LIST */
+	std::cout << GREEN << "Creating a simple list with almost same code :" << END << std::endl;
+	{
+		std::list<int> list;
+
+		list.push_back(1);
+		list.push_back(2);
+		list.push_back(3);
+		list.push_back(4);
+
+		std::list<int>::iterator it2;
+		std::list<int>::iterator ite2 = list.end();
+		for (it2 = list.begin(); it2 != ite2; it2++)
+		{
+			std::cout << *it2 << std::endl;
+		}
+	}
 	return 0;
 }
